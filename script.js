a64d2da4ff1f5f0f11a0f02a2226e1c927abfed5
Lawrence Weinstein <Larry4205@protonmail.com>
2022-06-04 15:27:51 -0700

    Added code to create 16 x 16 grid of sketch cells

---
+++ script.js
@@ -0,0 +1,12 @@
+//Add div to
+const myContainer = document.querySelector("#container");
+for(let i = 0; i < 16; i++) {
+    let row = document.createElement('div');
+    row.classList.add('sketchRow');
+    for (let j = 0; j < 16; j++) {
+        let cell = document.createElement('div');
+        cell.classList.add('sketchCell');
+        row.append(cell);
+    }
+    myContainer.append(row);
+}
